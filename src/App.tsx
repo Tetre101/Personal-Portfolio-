e19cbfa31cc4e3c1fbbbc21d36e8da4b532757ff
Tetre101 <basilpeter989@gmail.com>
2023-04-17 12:14:24 +0200

    initial setup completed

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      LET'S GET THIS STARTED
+    </div>
+  )
+}
+
+export default App
