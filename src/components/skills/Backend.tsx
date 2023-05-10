7f84d4ae77b485f74addd52961e6e0b91790e4d0
Tetre101 <basilpeter989@gmail.com>
2023-05-10 17:49:56 +0200

    completed About, skills info sections with styles

---
+++ src/components/skills/Backend.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+export const Backend = () => {
+    return (
+        <div className="skills__content">
+            <h3 className="skills__title">Backend Developer</h3>
+            <div className="skills__box">
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <i class='bx bxs-badge-check'></i>
+                        <div>
+                            <h3 className="skills__name">Node Js</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <i class='bx bxs-badge-check'></i>
+                        <div>
+                            <h3 className="skills__name">Express</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
+
+                    </div>
+                    <div className="skills__data">
+                        <i class='bx bxs-badge-check'></i>
+                        <div>
+                            <h3 className="skills__name">MySql</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <i class='bx bxs-badge-check'></i>
+                        <div>
+                            <h3 className="skills__name">Mongo DB</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <i class='bx bxs-badge-check'></i>
+                        <div>
+                            <h3 className="skills__name">GraphQl</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
+
+                    </div>
+                    <div className="skills__data">
+                        <i class='bx bxs-badge-check'></i>
+                        <div>
+                            <h3 className="skills__name">CI/CD</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
+
+                    </div>
+                    <div className="skills__data">
+                        <i class='bx bxs-badge-check'></i>
+                        <div>
+                            <h3 className="skills__name">Firebase</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
