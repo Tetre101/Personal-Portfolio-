e72a88cc7622207c07475eb4f60838c750fa9b34
Tetre101 <basilpeter989@gmail.com>
2023-05-13 15:06:19 +0200

    contact completed, used toast and formspree

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import { About } from './components/about/About'
 import { Skills } from './components/skills/Skills'
 import { Service } from './components/services/Service'
 import { Qualification } from './components/qualifications/Qualification'
+import { Contact } from './components/contact/Contact'
+// import { Toaster } from 'react-hot-toast'
 function App() {
-
-
   return (
     <div className="App">
+
       <Header />
       <main className="main">
         <Home />
@@ -17,6 +18,7 @@ function App() {
         <Skills />
         <Service />
         <Qualification />
+        <Contact />
       </main>
     </div>
   )
